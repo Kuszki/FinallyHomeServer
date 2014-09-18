3e0505e31a1fc2c4efac1c156758394abb99564d
Łukasz Dróżdż <l.drozdz@o2.pl>
2014-09-18 16:48:42 +0200

    update resources

--- obj/FinallyHome_Server_private.h
+++ obj/FinallyHome_Server_private.h
@@ -7,19 +7,19 @@
 #define FINALLYHOME_SERVER_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.0.345"
+#define VER_STRING	"1.0.0.348"
 #define VER_MAJOR	1
 #define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	345
+#define VER_BUILD	348
 #define COMPANY_NAME	"KuszkiDevGroup"
-#define FILE_VERSION	""
+#define FILE_VERSION	"1.0"
 #define FILE_DESCRIPTION	"FinallyHome inteligent home server"
-#define INTERNAL_NAME	""
-#define LEGAL_COPYRIGHT	""
-#define LEGAL_TRADEMARKS	""
-#define ORIGINAL_FILENAME	""
+#define INTERNAL_NAME	"Server"
+#define LEGAL_COPYRIGHT	"£ukasz 'Kuszki' Dró¿d¿"
+#define LEGAL_TRADEMARKS	"KuszkiDevGroup"
+#define ORIGINAL_FILENAME	"Server.exe"
 #define PRODUCT_NAME	"FinallyHome Server"
-#define PRODUCT_VERSION	""
+#define PRODUCT_VERSION	"1.0"
 
 #endif /*FINALLYHOME_SERVER_PRIVATE_H*/
