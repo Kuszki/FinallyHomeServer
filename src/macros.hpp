47d33ce1333d4020634acc04a4886f8475bab5b9
Łukasz Dróżdż <l.drozdz@o2.pl>
2014-09-19 20:33:28 +0200

    add macros header

---
+++ src/macros.hpp
@@ -0,0 +1,20 @@
+#ifndef _FINALLYHOMESERVER_MACROS_HPP
+#define _FINALLYHOMESERVER_MACROS_HPP
+
+#define PROMPT 		T("\r\n$: ")	//!< Znak zachęty.
+
+#define SOCK_SECTION	1			//!< Identyfikator sekcji krytycznej gniazda.
+#define CONS_SECTION	2			//!< Identyfikator sekcji krytycznej konsoli.
+#define CORE_SECTION	3			//!< Identyfikator sekcji krytycznej jądra.
+
+#define IF_DEBUG 		if (bDebug)	//!< Skrócone wywołanie konstrukcji warunkowej.
+
+#define CMD_UNKNOWN		0			//!< Identyfikator nieznanego polecenia.
+
+#define CMD_RCON		1			//!< Identyfikator polecenia "rcon".
+#define CMD_SET		2			//!< Identyfikator polecenia "set".
+#define CMD_GET		3			//!< Identyfikator polecenia "get".
+
+#define CMD_BYE		10			//!< Identyfikator polecenia "bye".
+
+#endif
